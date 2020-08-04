aa2e3e3a18ac7e4f7fd0a0286565c1ed5f19ba9d
lapptomi <lappetelainen.tomi@gmail.com>
2020-08-04 22:28:52 +0300

    Osa1 - tehtävä 1.8 tehty

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
+const Statistics = ({good, neutral, bad}) => {
   let all = good+neutral+bad
 
-  let positive = Number.isNaN(good/all)
+  let positive = Number.isNaN(good/all) 
     ? 0
     : (good * 100) / (good+neutral+bad)
    
@@ -18,19 +14,35 @@ const App = () => {
     : (good-bad)/all
 
   return (
+    <>
+      <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
+    </>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
     <div>
       <h1>give feedback</h1>
         <button onClick={() => setGood(good+1)}>good</button>
         <button onClick={() => setNeutral(neutral+1)}>neutral</button>
         <button onClick={() => setBad(bad+1)}>bad</button>
-      <h2>statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+      />
     </div>
   )
 }
