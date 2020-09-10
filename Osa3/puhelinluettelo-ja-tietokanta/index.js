fc89621540b7616d7cc7d4e407eee5896de2cdce
lapptomi <lappetelainen.tomi@gmail.com>
2020-09-10 22:46:52 +0300

    Osa3 - Tehtävät 3.13 ja 3.14 tehty

---
+++ Osa3/puhelinluettelo-ja-tietokanta/index.js
@@ -0,0 +1,64 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const Person = require('./models/person')
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(express.static('build'))
+
+
+app.get('/api/info', (request, response) => {
+  const personCount = persons.length
+  response.write(`Phonebook has info for ${0} people \n`)
+  response.write(Date())
+  response.end()
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(people => {
+    response.json(people)
+  })
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if ((body.name === undefined || body.number === undefined)) {
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+  })
+
+	person.save().then(savedPerson => {
+		response.json(savedPerson)
+	})
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(p => p.id != id)
+  response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
+})
