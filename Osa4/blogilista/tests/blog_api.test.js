f8afb9cc81bfce334c93bd3313365ec5de0aa708
lapptomi <lappetelainen.tomi@gmail.com>
2020-09-30 14:45:51 +0300

    Osa4 - Tehtävä 4.9 tehty

--- Osa4/blogilista/tests/blog_api.test.js
+++ Osa4/blogilista/tests/blog_api.test.js
@@ -28,18 +28,29 @@ beforeEach(async () => {
   await blogObject2.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
 
-test('GET request returns right amount of blogs from DB', async () => {
-  const response = await api.get('/api/blogs')
-  const blogsLength = response.body.length
-  expect(response.body).toHaveLength(2)
-  expect(response.body).toHaveLength(blogsLength)
+describe('blogs from database', () => {
+  test('are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test('returns right amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const blogsLength = response.body.length
+    expect(response.body).toHaveLength(2)
+    expect(response.body).toHaveLength(blogsLength)
+  })
+
+  test('has identifier named id instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.map(blog => {
+      expect(blog.id).toBeDefined()
+      expect(blog._id).toBeUndefined()
+    })
+  })
 })
 
 afterAll(() => {
