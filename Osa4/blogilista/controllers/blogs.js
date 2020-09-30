cbd13323f77de407181dbc1a9744d4564f621a08
lapptomi <lappetelainen.tomi@gmail.com>
2020-09-30 22:27:54 +0300

    Osa4 - Tehtävä 4.14 tehty

--- Osa4/blogilista/controllers/blogs.js
+++ Osa4/blogilista/controllers/blogs.js
@@ -11,10 +11,11 @@ blogsRouter.get('/', (request, response) => {
 
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
-  if (!blog.likes || blog.likes === undefined) blog.likes = 0
+  if (!blog.likes) blog.likes = 0
   if (!(blog.title && blog.url)) {
     return response.status(400).end()
   }
+  
   blog.save().then(result => {
       response.status(201).json(result)
     })
@@ -24,6 +25,28 @@ blogsRouter.delete('/:id', async (request, response, next) => {
   try {
     await Blog.findByIdAndDelete(request.params.id)
     return response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {    
+  const body = request.body
+  const id = request.params.id
+
+  try { 
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) return response.status(404).end()
+
+    const updatedBlog = {
+      title: !body.title ? blog.title : body.title,
+      author: !body.author ? blog.author : body.author,
+      url: !body.url ? blog.url : body.url,
+      likes: !body.likes ? blog.likes : body.likes,
+    }
+    
+    await Blog.findByIdAndUpdate(id, updatedBlog, { new: true })
+    response.json(updatedBlog)
   } catch (error) {
     next(error)
   }
