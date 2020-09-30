86153d8f97b1b351e5c1e2a43ca1eaa8ae9b16f4
lapptomi <lappetelainen.tomi@gmail.com>
2020-09-30 19:41:56 +0300

    Osa4 - Tehtävä 4.13 tehty

--- Osa4/blogilista/controllers/blogs.js
+++ Osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,7 @@
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const logger = require('../utils/logger')
 
 blogsRouter.get('/', (request, response) => {
   Blog.find({}).then(blogs => {
@@ -10,15 +11,22 @@ blogsRouter.get('/', (request, response) => {
 
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
-  
   if (!blog.likes || blog.likes === undefined) blog.likes = 0
   if (!(blog.title && blog.url)) {
     return response.status(400).end()
   }
-
   blog.save().then(result => {
       response.status(201).json(result)
     })
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    return response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
