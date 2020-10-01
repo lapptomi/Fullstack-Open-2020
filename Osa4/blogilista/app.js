83f3675d8fc780d8fc2030df43768762d15d4c9d
lapptomi <lappetelainen.tomi@gmail.com>
2020-10-01 19:44:19 +0300

    Osa4 - Tehtävä 4.15 tehty

--- Osa4/blogilista/app.js
+++ Osa4/blogilista/app.js
@@ -5,7 +5,9 @@ const cors = require('cors')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 logger.info('connecting to MongoDB...')
 
@@ -23,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
