efa16222d829c7a1459a329bf913ec3b147599d9
lapptomi <lappetelainen.tomi@gmail.com>
2020-10-03 17:54:40 +0300

    Osa5 tehtävä 5.2 tehty

--- Osa5/blogilista/blogilista-frontend/src/App.js
+++ Osa5/blogilista/blogilista-frontend/src/App.js
@@ -18,12 +18,25 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
 
       blogService.setToken(user.token)
       setUser(user)
@@ -33,6 +46,12 @@ const App = () => {
       window.alert('Wrong username or password')
     }
   }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+  }
+
 
   if (user === null) {
     return (
@@ -66,7 +85,12 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>
+          logout
+        </button>
+      </p>      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
