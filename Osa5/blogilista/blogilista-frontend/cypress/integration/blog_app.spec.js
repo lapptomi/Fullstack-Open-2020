ddb5e6a12433ff81eacabd49a9c8a6babd56d428
lapptomi <lappetelainen.tomi@gmail.com>
2020-10-06 21:09:39 +0300

    Osa5 tehtävä 5.18 tehty

--- Osa5/blogilista/blogilista-frontend/cypress/integration/blog_app.spec.js
+++ Osa5/blogilista/blogilista-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,35 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    // create here a user to backend
+    const testUser = {
+      username: "testusername", 
+      name: "testname", 
+      password: "testpassword"
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', testUser)
     cy.visit('http://localhost:3000')
   })
 
   it('Login from is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testusername')
+      cy.get('#password').type('testpassword')
+      cy.contains('login').click()
+
+      cy.contains('logout')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('wrongusername')
+      cy.get('#password').type('wrongpassword')
+      cy.contains('login').click()
+
+      cy.contains('Wrong username or password')
+    })
   })
 })
