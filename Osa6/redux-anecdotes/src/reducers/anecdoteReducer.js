0c25d9650583553a014300e4dc2cd67cdd663674
lapptomi <lappetelainen.tomi@gmail.com>
2020-10-10 16:57:33 +0300

    Osa6 tehtävä 6.15 tehty

--- Osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,4 @@
+import anecdoteService from '../services/anecdotes'
 
 
 export const addVoteTo = (id) => {
@@ -15,10 +16,13 @@ export const createAnecdote = (content) => {
 }
 
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
   }
 }
 
