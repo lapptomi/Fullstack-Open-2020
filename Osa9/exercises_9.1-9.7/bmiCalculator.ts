566736129a2c3b56653687c40789d82014532456
lapptomi <lappetelainen.tomi@gmail.com>
2020-12-17 16:55:16 +0200

    Osa9 - tehtävä 9.1 tehty

---
+++ Osa9/exercises_9.1-9.7/bmiCalculator.ts
@@ -0,0 +1,16 @@
+
+
+const calculateBmi = (height: number, weight: number): string => {
+    const result: number = weight / (Math.pow(180 / 100, 2))
+
+    if (result < 15) return 'Very severely underweight'
+    else if (result < 16) return 'Severely underweight'
+    else if (result < 18.5) return 'Underweight'
+    else if (result < 25) return 'Normal (healthy weight)'
+    else if (result < 30) return 'Overweight'
+    else if (result < 35) return 'Obese Class I (Moderately obese)'
+    else if (result < 40) return 'Obese Class II (Severely obese)'
+    else return 'Obese Class III (Very severely obese)'
+}
+
+console.log(calculateBmi(180, 74))
