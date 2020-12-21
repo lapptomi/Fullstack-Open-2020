fd3e6e3ecf4d171e24db308936a31d32ddbb6452
lapptomi <lappetelainen.tomi@gmail.com>
2020-12-21 20:51:00 +0200

    Osa9 - tehtävä 9.5 tehty

--- Osa9/exercises_9.1-9.7/index.ts
+++ Osa9/exercises_9.1-9.7/index.ts
@@ -1,10 +1,25 @@
 import express from 'express'
+import calculateBmi from './bmiCalculator'
+
 const app = express()
 
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
 })
+
+app.get('/bmi', (req, res) => {
+  const height: number = Number(req.query.height)
+  const weight: number = Number(req.query.weight)
+
+  if (!height || !weight) {
+    return res.json({ error: "malformatted parameters" })
+  }
+
+  const bmi = calculateBmi(height, weight)
+  return res.json({ weight, height, bmi })
+})
+
 
 const PORT = 3003
 app.listen(PORT, () => {
