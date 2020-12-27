e1abb4bdf43782f5bf84370ebbaef25024d08ac3
lapptomi <lappetelainen.tomi@gmail.com>
2020-12-27 16:17:57 +0200

    Osa9 - tehtävä 9.10 tehty

--- Osa9/patientor/patientor-backend/src/index.ts
+++ Osa9/patientor/patientor-backend/src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
